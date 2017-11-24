a59961fe3e7b0aea6c750000893a5c845dc0c0fc
Joey Cho <cjy12182000@naver.com>
2017-11-24 13:34:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 
+class Driver{
+  constructor(name, day){
+    this.name = name;
+    this.day = day;
+    this.startDate = new Date(this.day);
+  }
+
+
+  yearsExperienceFromBeginningOf(year){
+    return year - (this.startDate.getFullYear()+1)
+  }
+
+}
+class Route{
+  constructor(beginningLocation, endingLocation){
+    this.begin = beginningLocation
+    this.end = endingLocation
+  }
+  blocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    let hordis = Math.abs(eastWest.indexOf(this.begin[horizontal])-eastWest.indexOf(this.end[horizontal]))
+    let vertdis = Math.abs(parseInt(this.end[vertical].split("th")[0])-parseInt(this.begin[vertical].split("th")[0]))
+    return hordis+vertdis
+
+//    return hordis
+  }
+}
